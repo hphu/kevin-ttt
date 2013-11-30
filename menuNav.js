8840136932bffc3d7dfccf65b209a49c5576a981
Henry Phu <hphu@bu.edu>
2013-11-30 15:29:34 -0500

    removed session, handled by registration

--- menuNav.js
+++ menuNav.js
@@ -1,10 +1,9 @@
-var playerid;
 
 function friendMatch(){
   window.location.href='friendlist.html';
 }
 function rankedMatch(){
-  window.location.href='gameboard.html';
+  window.location.href='game.html';
 }
 function compMatch(){
   window.location.href='compmatch.html';
@@ -16,20 +15,3 @@ function record(){
   window.location.href='record.html';
 }
 
-function getsessionid(){ //get my player id
-  var id;
-    $.ajax({
-	    type: 'POST',
-	    url: 'session.php',
-      async:false,
-	    success: function(data) {
-	    	playerid=data;
-	    }
-	});
-}
-
-$(document).ready( function(){
-  getsessionid(); //assign playerid = to the session id
-  });
-  
-  
